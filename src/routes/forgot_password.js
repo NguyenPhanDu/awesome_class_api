a3982929830d6f568ccdc030f07cc679741ee488
Du Nguyễn <nguyenphanduu12@gmail.com>
2021-05-08 21:10:17 +0700

    [DU][BE] forgot password

---
+++ src/routes/forgot_password.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const forgotPasswordController = require('../app/api/controllers/ForgotPasswordController');
+
+router.post('/send-mail',forgotPasswordController.sendReSetMail);
+router.post('/',forgotPasswordController.handleForgotPassword);
+
+module.exports = router;
