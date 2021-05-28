c426ae5df59992a3f91bd5e69e5418711fd2eec5
Du Nguyễn <nguyenphanduu12@gmail.com>
2021-05-28 14:29:22 +0700

    [DU][BE]  add class, user imgae model

--- src/routes/modules/user_profile.js
+++ src/routes/modules/user_profile.js
@@ -4,8 +4,8 @@ const router = express.Router();
 const {verifyToken} = require('../../app/middlewares/authJwt');
 const UserProfileController = require('../../app/api/controllers/UserProfileController');
 
-router.use(verifyToken);
 router.post('/get-user-profile', UserProfileController.getUserProfile);
 router.post('/update-user-profile',UserProfileController.updateUserProfile);
+router.post('/update-avatar', UserProfileController.updateAvatar);
 router.post('/update-avatar', UserProfileController.updateAvatar);
 module.exports = router;
