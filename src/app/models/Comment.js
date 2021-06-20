44be9e8cc24e713ed906bf7a490c0e98627a8da8
Du Nguyễn <nguyenphanduu12@gmail.com>
2021-06-20 16:47:35 +0700

    [DU][BE] fix bug comment

--- src/app/models/Comment.js
+++ src/app/models/Comment.js
@@ -23,7 +23,7 @@ const CommentSchema = Schema(
         onModel: {
             type: String,
             require: true,
-            enum: ['ClassNews', 'ClassNotification']
+            enum: ['ClassNews', 'ClassHomework']
         },
         is_delete: {
             type: Boolean,
