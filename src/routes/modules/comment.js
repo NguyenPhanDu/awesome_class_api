ad57f589764c6493860af7e4267915d26b98ed1c
Du Nguyễn <nguyenphanduu12@gmail.com>
2021-10-13 10:19:00 +0700

    add

--- src/routes/modules/comment.js
+++ src/routes/modules/comment.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const {verifyToken} = require('../../app/middlewares/authJwt');
 const CommentController = require('../../app/api/controllers/CommentController');
-
+//
 router.use(verifyToken);
 router.post('/create', CommentController.create);
 router.post('/update', CommentController.update);
