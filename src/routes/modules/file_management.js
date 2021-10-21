10663361c26ad54d664248293abe8c1baca67ff0
Du Nguyễn <nguyenphanduu12@gmail.com>
2021-10-21 14:53:50 +0700

    [DU][BE] update

--- src/routes/modules/file_management.js
+++ src/routes/modules/file_management.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const {verifyToken} = require('../../app/middlewares/authJwt');
 const FileManagementController = require('../../app/api/controllers/ClassFileManagementController');
 const UserPermissionMiddleware = require('../../app/middlewares/userPermission')
-//router.use(verifyToken);
+router.use(verifyToken);
 router.post('/show-all-file', FileManagementController.showAllFileInClass)
-//router.post('/test',bb.limitMemberInClass,FileManagementController.testThoi);
+//router.post('/test',UserPermissionMiddleware.limitMemberInClass,FileManagementController.testThoi);
 
 module.exports = router;
