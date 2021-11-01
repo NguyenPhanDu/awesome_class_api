940bfbb473efb41d48b66a3105d346e3d148da56
Du Nguyễn <nguyenphanduu12@gmail.com>
2021-11-01 22:05:35 +0700

    [DU][fix]

--- src/app/mailers/sendEmailActivate/sendEmailActivate.js
+++ src/app/mailers/sendEmailActivate/sendEmailActivate.js
@@ -6,10 +6,6 @@ const { google } = require('googleapis');
 
 async function sendActiveMail(user){
     try{
-        const viewPath = path.resolve('./src/resources/views/email')
-        console.log("viewPath: ",viewPath)
-        const viewPath2 = path.join(__dirname,'../../../resources/views/email')
-        console.log("viewPath2: ",viewPath2)
         const oauth2Client = new google.auth.OAuth2(
             process.env.GD_CLIENT_ID,
             process.env.GD_CLIENT_SECRET,
