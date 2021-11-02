eabf79e58e261ebd6e6b3859f44fd770d682d16e
Du Nguyễn <nguyenphanduu12@gmail.com>
2021-11-02 21:37:11 +0700

    [DU][BE] fix

--- src/app/api/controllers/MutilChoiceController.js
+++ src/app/api/controllers/MutilChoiceController.js
@@ -28,6 +28,7 @@ const Test = require('../../models/Test')
 // ];
 async function create(req, res){
     try{
+        console.log(req.body)
         let reqQuestions = await JSON.parse(req.body.questions)
         let reqStudent = await JSON.parse(req.body.emails);
         let reqCategory = await JSON.parse(req.body.category);
@@ -51,194 +52,89 @@ async function create(req, res){
                                 .populate('role');
         let userRole = classMember.role.id_class_role;
         if(userRole == 1 || userRole == 3){
-            if(reqCategory == null){
-                let homework = MutilChoiceHomework({
-                    title: req.body.title,
-                    description: req.body.description,
-                    start_date: req.body.start_date,
-                    deadline: req.body.deadline,
-                    total_scores: reqTotalScore,
-                    homework_type: mongoose.Types.ObjectId(homeWorkTypeId),
-                    create_by: mongoose.Types.ObjectId(userId),
-                    questions: reqQuestions
-                });
-                let newHomework = await homework.save();
-                let classHomework = await ClassHomework.create({
-                    class: mongoose.Types.ObjectId(classId),
-                    homework: mongoose.Types.ObjectId(newHomework._id),
-                    onModel: 'MutilChoiceHomework'
-                });
-                if(req.files){
-                    if(req.files.length> 0){
-                        for(let i = 0; i < req.files.length; i++){
-                            await FolerServices.uploadFileCreateMutilChoiceHomeWork(userId, classId, classHomework,req.files[i], newHomework._id);
-                        }
+            let homework = MutilChoiceHomework({
+                title: req.body.title,
+                description: req.body.description,
+                start_date: req.body.start_date,
+                deadline: req.body.deadline,
+                total_scores: reqTotalScore,
+                homework_type: mongoose.Types.ObjectId(homeWorkTypeId),
+                create_by: mongoose.Types.ObjectId(userId),
+                questions: reqQuestions
+            });
+            let newHomework = await homework.save();
+            let classHomework = await ClassHomework.create({
+                class: mongoose.Types.ObjectId(classId),
+                homework: mongoose.Types.ObjectId(newHomework._id),
+                onModel: 'MutilChoiceHomework'
+            });
+            if(req.files){
+                if(req.files.length> 0){
+                    for(let i = 0; i < req.files.length; i++){
+                        await FolerServices.uploadFileCreateMutilChoiceHomeWork(userId, classId, classHomework,req.files[i], newHomework._id);
                     }
                 }
-                if(reqStudent.length > 0){
-                    let arrayUserId = [];
-                    for(let i = 0; i < reqStudent.length; i++){
-                        let user =  await User.findOne({email: reqStudent[i] })
-                        arrayUserId.push(user._id);
-                        listIdReceiver.push(user._id);
-                    }
-                    for(let i = 0; i< arrayUserId.length; i++){
-                        await HomeworkAssign.create({
-                            user: mongoose.Types.ObjectId(arrayUserId[i]),
-                            class: mongoose.Types.ObjectId(classId),
-                            homework: mongoose.Types.ObjectId(newHomework._id),
-                            onModel: 'MutilChoiceHomework'
-                        });
-                    }
-                    // giáo viên trong lớp trừ bản thân
-                    const allTeacherInClass = await ClassMember.find({ class: mongoose.Types.ObjectId(classId), role: mongoose.Types.ObjectId(classRoleTeacherId) ,user: { $ne: mongoose.Types.ObjectId(userId) } });
-                    JSON.parse(JSON.stringify(allTeacherInClass));
-                    // push vào mảng học sinh
-                    for(let i = 0; i < allTeacherInClass.length; i++){
-                        listIdReceiver.push(allTeacherInClass[i].user);
-                    }
-                }
-                else{
-                    let arrStudentInClass = await ClassMember.find({ class: mongoose.Types.ObjectId(classId), role: mongoose.Types.ObjectId(classRoleStudentId), is_delete: false, status: 1 })
-                    if(arrStudentInClass.length > 0){
-                        for(let i =0; i< arrStudentInClass.length; i++){
-                            await HomeworkAssign.create({
-                                user: mongoose.Types.ObjectId(arrStudentInClass[i].user),
-                                class: mongoose.Types.ObjectId(classId),
-                                homework: mongoose.Types.ObjectId(newHomework._id),
-                                onModel: 'MutilChoiceHomework'
-                            });
-                        };
-                    }
-                    // tất cả thành viên trong class trừ bản thân
-                    let allMemberInClass = await ClassMember.find({ class: mongoose.Types.ObjectId(classId), user: { $ne: mongoose.Types.ObjectId(userId) } });
-                    JSON.parse(JSON.stringify(allMemberInClass));
-                    listIdReceiver  = allMemberInClass.map(item => {
-                        return item.user
-                    });
-
-                };
-                const homeworkCreated = await MutilChoiceHomework.findById(newHomework._id)
-                                .populate("homework_type", "-_id -__v")
-                                .populate("create_by", "-_id -__v -password")
-                                .populate("homework_category", "-_id -__v")
-                                .populate("document", "name viewLink downloadLink size id_files");
-                res.json({
-                    success: true,
-                    message: "Create homework successfull!",
-                    data: homeworkCreated,
-                    res_code: 200,
-                    res_status: "CREATE_SUCCESSFULLY"
-                });
-                
-                await NotificationController.exerciseNotify(classHomework._id, userId, listIdReceiver, 1);
             }
-            else{
-                let flag = false;
-                let allCategoryInClass = await HomeworkCategory.find({class: mongoose.Types.ObjectId(classId), is_delete: false})
-                const allCategoryInClassLengnt = allCategoryInClass.length
-                if(allCategoryInClassLengnt > 0){
-                    for(let i = 0 ;i< allCategoryInClassLengnt; i++){
-                        if(reqCategory.title.toLowerCase() == allCategoryInClass[0].title.toLowerCase()){
-                            flag = true;
-                            res.json({
-                                success: false,
-                                message: 'This category already exist!',
-                                res_code: 422,
-                                res_status: "SERVER_ERROR"
-                            });
-                            return;
-                        }
-                    }
+            if(reqStudent.length > 0){
+                let arrayUserId = [];
+                for(let i = 0; i < reqStudent.length; i++){
+                    let user =  await User.findOne({email: reqStudent[i] })
+                    arrayUserId.push(user._id);
+                    listIdReceiver.push(user._id);
                 }
-                if(flag == false){
-                    const newHomeworkCategory = await HomeworkCategory.create({
-                        title: reqCategory.title,
-                        user: mongoose.Types.ObjectId(userId),
-                        class: mongoose.Types.ObjectId(classId)
-                    });
-                    let homework = MutilChoiceHomework({
-                        title: req.body.title,
-                        description: req.body.description,
-                        start_date: req.body.start_date,
-                        deadline: req.body.deadline,
-                        total_scores: reqTotalScore,
-                        homework_type: mongoose.Types.ObjectId(homeWorkTypeId),
-                        homework_category: mongoose.Types.ObjectId(newHomeworkCategory._id),
-                        create_by: mongoose.Types.ObjectId(userId),
-                        questions: reqQuestions
-                    });
-                    let newHomework = await homework.save();
-                    let classHomework = await ClassHomework.create({
+                for(let i = 0; i< arrayUserId.length; i++){
+                    await HomeworkAssign.create({
+                        user: mongoose.Types.ObjectId(arrayUserId[i]),
                         class: mongoose.Types.ObjectId(classId),
                         homework: mongoose.Types.ObjectId(newHomework._id),
                         onModel: 'MutilChoiceHomework'
                     });
-                    if(req.files){
-                        if(req.files.length> 0){
-                            for(let i = 0; i < req.files.length; i++){
-                                await FolerServices.uploadFileCreateMutilChoiceHomeWork(userId, classId, classHomework,req.files[i], newHomework._id);
-                            }
-                        }
-                    }
-                    if(reqStudent.length > 0){
-                        let arrayUserId = [];
-                        for(let i = 0; i < reqStudent.length; i++){
-                            let user =  await User.findOne({email: reqStudent[i] })
-                            arrayUserId.push(user._id)
-                            listIdReceiver.push(user._id);
-                        }
-                        for(let i = 0; i< arrayUserId.length; i++){
-                            await HomeworkAssign.create({
-                                user: mongoose.Types.ObjectId(arrayUserId[i]),
-                                class: mongoose.Types.ObjectId(classId),
-                                homework: mongoose.Types.ObjectId(newHomework._id),
-                                onModel: 'MutilChoiceHomework'
-                            })
-                        }
-                        // giáo viên trong lớp trừ bản thân
-                        const allTeacherInClass = await ClassMember.find({ class: mongoose.Types.ObjectId(classId), role: mongoose.Types.ObjectId(classRoleTeacherId) ,user: { $ne: mongoose.Types.ObjectId(userId) } });
-                        JSON.parse(JSON.stringify(allTeacherInClass));
-                        // push vào mảng học sinh
-                        for(let i = 0; i < allTeacherInClass.length; i++){
-                            listIdReceiver.push(allTeacherInClass[i].user);
-                        }
-                    }
-                    else{
-                        let arrStudentInClass = await ClassMember.find({ class: mongoose.Types.ObjectId(classId), role: mongoose.Types.ObjectId(classRoleStudentId), is_delete: false })
-                        if(arrStudentInClass > 0){
-                            for(let i =0; i< arrStudentInClass.length; i++){
-                                await HomeworkAssign.create({
-                                    user: mongoose.Types.ObjectId(arrStudentInClass[i].user),
-                                    class: mongoose.Types.ObjectId(classId),
-                                    homework: mongoose.Types.ObjectId(newHomework._id),
-                                    onModel: 'MutilChoiceHomework'
-                                })
-                            };
-                        }
-                        // tất cả thành viên trong class trừ bản thân
-                        let allMemberInClass = await ClassMember.find({ class: mongoose.Types.ObjectId(classId), user: { $ne: mongoose.Types.ObjectId(userId) } });
-                        JSON.parse(JSON.stringify(allMemberInClass));
-                        listIdReceiver  = allMemberInClass.map(item => {
-                            return item.user
-                        });
-
-                    };
-                    const homeworkCreated = await MutilChoiceHomework.findById(newHomework._id)
-                                    .populate("homework_type", "-_id -__v")
-                                    .populate("create_by", "-_id -__v -password")
-                                    .populate("homework_category", "-_id -__v")
-                                    .populate("document", "name viewLink downloadLink size id_files");
-                    res.json({
-                        success: true,
-                        message: "Create homework successfull!",
-                        data: homeworkCreated,
-                        res_code: 200,
-                        res_status: "CREATE_SUCCESSFULLY"
-                    })
-                    await NotificationController.exerciseNotify(classHomework._id, userId, listIdReceiver, 1);
+                }
+                // giáo viên trong lớp trừ bản thân
+                const allTeacherInClass = await ClassMember.find({ class: mongoose.Types.ObjectId(classId), role: mongoose.Types.ObjectId(classRoleTeacherId) ,user: { $ne: mongoose.Types.ObjectId(userId) } });
+                JSON.parse(JSON.stringify(allTeacherInClass));
+                // push vào mảng học sinh
+                for(let i = 0; i < allTeacherInClass.length; i++){
+                    listIdReceiver.push(allTeacherInClass[i].user);
                 }
             }
+            else{
+                console.log("gửi toàn bộ sinh viên trong lớp")
+                let arrStudentInClass = await ClassMember.find({ class: mongoose.Types.ObjectId(classId), role: mongoose.Types.ObjectId(classRoleStudentId), is_delete: false, status: 1 })
+                console.log("danh sách sinh viên: ");
+                console.log(arrStudentInClass)
+                if(arrStudentInClass.length > 0){
+                    for(let i =0; i< arrStudentInClass.length; i++){
+                        await HomeworkAssign.create({
+                            user: mongoose.Types.ObjectId(arrStudentInClass[i].user),
+                            class: mongoose.Types.ObjectId(classId),
+                            homework: mongoose.Types.ObjectId(newHomework._id),
+                            onModel: 'MutilChoiceHomework'
+                        });
+                    };
+                }
+                // tất cả thành viên trong class trừ bản thân
+                let allMemberInClass = await ClassMember.find({ class: mongoose.Types.ObjectId(classId), user: { $ne: mongoose.Types.ObjectId(userId) } });
+                JSON.parse(JSON.stringify(allMemberInClass));
+                listIdReceiver  = allMemberInClass.map(item => {
+                    return item.user
+                });
+
+            };
+            const homeworkCreated = await MutilChoiceHomework.findById(newHomework._id)
+                            .populate("homework_type", "-_id -__v")
+                            .populate("create_by", "-_id -__v -password")
+                            .populate("homework_category", "-_id -__v")
+                            .populate("document", "name viewLink downloadLink size id_files");
+            res.json({
+                success: true,
+                message: "Create homework successfull!",
+                data: homeworkCreated,
+                res_code: 200,
+                res_status: "CREATE_SUCCESSFULLY"
+            });
+            
+            await NotificationController.exerciseNotify(classHomework._id, userId, listIdReceiver, 1);
         }
         else{
             return res.json({
