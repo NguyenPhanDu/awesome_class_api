ca4cb27290501f1a37a1b5a74ec3fc99616cb244
Du Nguyễn <nguyenphanduu12@gmail.com>
2021-11-06 22:29:50 +0700

    [DU][BE] fix

--- src/app/mailers/sendEmailActivate/sendEmailResetPass.js
+++ src/app/mailers/sendEmailActivate/sendEmailResetPass.js
@@ -4,24 +4,26 @@ const hbs = require('nodemailer-express-handlebars');
 const { google } = require('googleapis');
 async function sendResetPasswordMail(req,user){
     try{
-        const oauth2Client = new google.auth.OAuth2(
-            process.env.GD_CLIENT_ID,
-            process.env.GD_CLIENT_SECRET,
-            process.env.GD_REDIRECT_URI
-        );
+        // const oauth2Client = new google.auth.OAuth2(
+        //     process.env.GD_CLIENT_ID,
+        //     process.env.GD_CLIENT_SECRET,
+        //     process.env.GD_REDIRECT_URI
+        // );
         
-        oauth2Client.setCredentials({refresh_token: process.env.MAIL_REFESH_TOKEN});
-        const accessToken = await oauth2Client.getAccessToken()
+        // oauth2Client.setCredentials({refresh_token: process.env.MAIL_REFESH_TOKEN});
+        // const accessToken = await oauth2Client.getAccessToken()
 
         let transport = nodemailer.createTransport({
-            service: 'gmail',
+            host: 'smtp.gmail.com',
+            port: 465,
+            secure: true,
             auth: {
-                type: 'OAuth2',
                 user: 'awesomeclass.work@gmail.com',
-                clientId: process.env.GD_CLIENT_ID,
-                clientSecret: process.env.GD_CLIENT_SECRET,
-                refreshToken: process.env.MAIL_REFESH_TOKEN,
-                accessToken: accessToken
+                pass: 'du0336685195'
+                // clientId: process.env.GD_CLIENT_ID,
+                // clientSecret: process.env.GD_CLIENT_SECRET,
+                // refreshToken: process.env.MAIL_REFESH_TOKEN,
+                // accessToken: accessToken
             },
         });
 
