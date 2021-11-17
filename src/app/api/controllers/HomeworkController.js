5e37599d420c8b07786a6e7bb6c26885ae6e28a3
Du Nguyễn <nguyenphanduu12@gmail.com>
2021-11-17 16:08:20 +0700

    [DU] fix

--- src/app/api/controllers/HomeworkController.js
+++ src/app/api/controllers/HomeworkController.js
@@ -22,9 +22,9 @@ class HomeWorkController{
         try{
             let reqStudent = await JSON.parse(req.body.emails);
             let reqCategory = await JSON.parse(req.body.category);
-            let reqTotalScore = await JSON.parse(req.body.total_scores);
-            console.log(reqTotalScore);
-            console.log(typeof(reqTotalScore));
+            //let reqTotalScore = await JSON.parse(req.body.total_scores);
+            console.log(req.body.total_scores);
+            console.log(typeof(req.body.total_scores));
             if(req.body.deadline == 'null'){
                 req.body.deadline = null;
             }
